4a30abc156973904ce6ce61fe04263d09a4052e5
Eugene Kinyanjui <102164326+eugenewainaina@users.noreply.github.com>
2023-09-19 18:37:17 +0300

    .cmd file to be executed in windows terminal
    in the terminal, navigate to the path the file has downloaded 
    
    execute this command: 
    unicat.cmd

---
+++ unicat.cmd
@@ -0,0 +1,153 @@
+@ECHO off
+
+:: work on colors
+:: escape sequences to work on feet using ^| for |
+:: add movement to the tail
+
+CLS
+
+ping 168.192.255.253 /n 1 /w 800 >nul
+
+ECHO                                                           UNICAT
+
+ECHO.
+ECHO.
+ECHO. 
+ECHO.                                                      .             .
+ECHO                                                       !\           /!            
+ECHO                                                       !;\_-_-_-_-_/;!            
+ECHO                                                       !             !            
+ECHO                                                      /  (I)     (I)  \          
+ECHO                                                      !       w       !        
+ECHO                                                     /       )v(       \    
+ECHO                                                    /                   \ 
+ECHO                                                  ./                     \.
+ECHO                                                 /#                        \
+ECHO                                                /###                        \
+ECHO                                                /####                       \
+ECHO                                               /#########         ####       \
+ECHO                                               /#######          ######      \
+ECHO                                               !                  ######     !
+ECHO                                               \##        ##        #####    /
+ECHO                                          ..    \###     ####         ##    /
+ECHO                                        .`#``.  \####    #####              /
+ECHO                                       .#.` `#`. \###     ####             /
+ECHO                                       .`#.  `#`. \##                     /
+ECHO                                        ``   `#`.  \.     /``)        /``)
+ECHO                                               \#\___\   {o o}       {o o}
+ECHO                                                \######_{O   O}`````{O   O}
+ECHO                                                  ````````````      ``````
+ECHO.
+
+
+ping 168.192.255.253 /n 1 /w 800 >nul
+
+CLS
+
+ECHO                                                           UNICAT
+
+ECHO.
+ECHO.
+ECHO. 
+ECHO.                                                      .             .
+ECHO                                                       !\           /!            (````````````````)
+ECHO                                                       !;\_-_-_-_-_/;!            (                )
+ECHO                                                       !             !            (                )
+ECHO                                                      /  (I)     (I)  \          _``````````````````
+ECHO                                                      !       w       !        _`
+ECHO                                                     /       )v(       \    --`
+ECHO                                                    /                   \ 
+ECHO                                                  ./                     \.
+ECHO                                                 /#                        \
+ECHO                                                /###                        \
+ECHO                                                /####                       \
+ECHO                                               /#########         ####       \
+ECHO                                               /#######          ######      \
+ECHO                                               !                  ######     !
+ECHO                                               \##        ##        #####    /
+ECHO                                          ..    \###     ####         ##    /
+ECHO                                        .`#``.  \####    #####              /
+ECHO                                       .#.` `#`. \###     ####             /
+ECHO                                       .`#.  `#`. \##                     /
+ECHO                                        ``   `#`.  \.     /``)        /``)
+ECHO                                               \#\___\   {o o}       {o o}
+ECHO                                                \######_{O   O}`````{O   O}
+ECHO                                                  ````````````      ``````
+ECHO.
+
+
+
+ping 168.192.255.253 /n 1 /w 800 >nul
+
+CLS
+
+ECHO                                                           UNICAT
+
+ECHO.
+ECHO.
+ECHO. 
+ECHO.                                                      .             .
+ECHO                                                       !\           /!            (````````````````)
+ECHO                                                       !;\_-_-_-_-_/;!            (    MEOW        )
+ECHO                                                       !             !            (                )
+ECHO                                                      /  (I)     (I)  \          _``````````````````
+ECHO                                                      !       w       !        _`
+ECHO                                                     /       )v(       \    --`
+ECHO                                                    /                   \ 
+ECHO                                                  ./                     \.
+ECHO                                                 /#                        \
+ECHO                                                /###                        \
+ECHO                                                /####                       \
+ECHO                                               /#########         ####       \
+ECHO                                               /#######          ######      \
+ECHO                                               !                  ######     !
+ECHO                                               \##        ##        #####    /
+ECHO                                          ..    \###     ####         ##    /
+ECHO                                        .`#``.  \####    #####              /
+ECHO                                       .#.` `#`. \###     ####             /
+ECHO                                       .`#.  `#`. \##                     /
+ECHO                                        ``   `#`.  \.     /``)        /``)
+ECHO                                               \#\___\   {o o}       {o o}
+ECHO                                                \######_{O   O}`````{O   O}
+ECHO                                                  ````````````      ``````
+ECHO.
+
+
+ping 168.192.255.253 /n 1 /w 800 >nul
+
+CLS
+
+ECHO                                                           UNICAT
+
+ECHO.
+ECHO.
+ECHO. 
+ECHO.                                                      .             .
+ECHO                                                       !\           /!            (````````````````)
+ECHO                                                       !;\_-_-_-_-_/;!            (    MEOW        )
+ECHO                                                       !             !            (       MEOW     )
+ECHO                                                      /  (I)     (I)  \          _``````````````````
+ECHO                                                      !       w       !        _`
+ECHO                                                     /       )v(       \    --`
+ECHO                                                    /                   \ 
+ECHO                                                  ./                     \.
+ECHO                                                 /#                        \
+ECHO                                                /###                        \
+ECHO                                                /####                       \
+ECHO                                               /#########         ####       \
+ECHO                                               /#######          ######      \
+ECHO                                               !                  ######     !
+ECHO                                               \##        ##        #####    /
+ECHO                                          ..    \###     ####         ##    /
+ECHO                                        .`#``.  \####    #####              /
+ECHO                                       .#.` `#`. \###     ####             /
+ECHO                                       .`#.  `#`. \##                     /
+ECHO                                        ``   `#`.  \.     /``)        /``)
+ECHO                                               \#\___\   {o o}       {o o}
+ECHO                                                \######_{O   O}`````{O   O}
+ECHO                                                  ````````````      ``````
+ECHO.
+
+
+
+ping 168.192.255.253 /n 1 /w 3000 >nul
